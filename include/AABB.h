6d3912635c04f1edf78927330b178dd79ff40ca0
libn <libn@shanghaitech.edu.cn>
2023-01-02 15:53:19 +0800

    add interpolation

--- include/AABB.h
+++ include/AABB.h
@@ -6,15 +6,60 @@
 struct AABB {
     Vec3f lower_bnd;
     Vec3f upper_bnd;
-    explicit AABB(const floatGrid::Ptr& grid);
+
+    explicit AABB(const floatGrid::Ptr &grid);
 
     bool intersect(const Ray &ray, float &t0, float &t1) const;
 };
 
-struct vdbGrid{
+struct vdbGrid {
     floatGrid::Ptr grid;
     AABB aabb;
-    explicit vdbGrid(const floatGrid::Ptr& grid): grid(grid), aabb(grid) {};
+    float dx;
+
+
+
+    explicit vdbGrid(const floatGrid::Ptr &grid) : grid(grid), aabb(grid) {
+        dx = float(grid->transform().voxelSize()[0]);
+//        print all value in grid
+
+        for (auto iter = grid->cbeginValueOn(); iter; ++iter) {
+            std::cout << iter.getValue() << std::endl;
+        }
+    };
+
+    [[nodiscard]] float interpolation(const Vec3f &pos) const {
+        Vec3f local_pos = grid->transform().worldToIndex(pos);
+        Vec3f lower = Vec3f(floor(local_pos.x()), floor(local_pos.y()), floor(local_pos.z()));
+        Vec3f upper = Vec3f(ceil(local_pos.x()), ceil(local_pos.y()), ceil(local_pos.z()));
+        std::vector<Vec3f> Corners{Vec3f(lower.x(), lower.y(), lower.z()),
+                                   Vec3f(lower.x(), lower.y(), upper.z()),
+                                   Vec3f(lower.x(), upper.y(), lower.z()),
+                                   Vec3f(lower.x(), upper.y(), upper.z()),
+                                   Vec3f(upper.x(), lower.y(), lower.z()),
+                                   Vec3f(upper.x(), lower.y(), upper.z()),
+                                   Vec3f(upper.x(), upper.y(), lower.z()),
+                                   Vec3f(upper.x(), upper.y(), upper.z())};
+        std::vector<float> weights;
+        for (auto &corner: Corners) {
+            corner = grid->transform().indexToWorld(corner);
+            float weight = 1;
+            for (int i = 0; i < 3; i++) {
+                weight *= float(fmax((1 - fabs(corner[i] - pos[i]) / dx), 0.0));
+            }
+            weights.push_back(weight);
+        }
+        float numerator = 0;
+        for (int i = 0; i <= 8; ++i) {
+            numerator += weights[i] * grid->tree().getValue(openvdb::Coord(Vec3i(Corners[i])));
+        }
+        float denominator = 0;
+        for (int i = 0; i <= 8; ++i) {
+            denominator += weights[i];
+        }
+        return numerator / denominator;
+    }
+
 };
 
 
